04f0b880e6f959c03d939b34765446b3cd3ce60b
Daniel Urra <danielurra75@gmail.com>
2023-06-16 17:51:11 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-/**  Use the DOM method document.getElementById()
- to access the element and its attributes
- onclick event handler, changes square color 
-*/
-    
-function paint(color) {
-    const square = document.getElementById('squareID');
-    square.style = `background-color:${color}`;
-  }
